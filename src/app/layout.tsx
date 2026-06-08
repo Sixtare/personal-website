ba8bb88e6effeb6fab3ecb0ea092626a071ebd7a
Matheus Gilla Sestare <mthsestare@hotmail.com>
2026-06-08 19:45:13 -0300

    fix: update title in metadata for clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Matheus Sestare - Fullstack Developer",
+  title: "Matheus Sestare - Developer Portfolio",
   description: "Portfolio",
 };
 
